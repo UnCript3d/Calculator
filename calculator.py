775891350a35d90c97f2c51381415363579f1190
UnCript3d <sksrdier@gmail.com>
2025-04-14 21:27:41 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,24 @@
+class calculator:
+    def start():
+        startup=input("What operation will you like to do user? {+,-,/,*}")
+        if startup=="+":
+            x=int(input("Input your first number"))
+            y=int(input("Input your secound number"))
+            z=x+y
+            print(z)
+
+        elif startup=='-':
+                x=int(input("Input your first number"))
+                y=int(input("Input your secound number"))
+                z=x-y
+
+        elif startup=='/':
+             x=int(input("Input your first number"))
+             y=int(input("Input your secound number"))
+             z=x/y
+        elif startup=='*':
+             x=int(input("Input your first number"))
+             y=int(input("Input your secound number"))
+             z=x*y
+                
+calculator.start()            
